00b8988edd1ac164c1a302f9c0480ed016305413
TGCAR <don.meln2013@yandex.ru>
2024-12-10 23:58:18 +0300

    HomeWork8ObjectClass

---
+++ src/Author.java
@@ -0,0 +1,26 @@
+public class Author {
+    private final String surName;
+    private final String firstName;
+
+
+    public Author(String surName, String firstName) {
+        this.surName = firstName;
+        this.firstName = surName;
+    }
+
+    public String getSurName() {
+        return this.surName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+//    public firstName() {
+//        return this.firstName();
+}
+
+//    public surName() {
+//        return this.surName();
+
+
